1fb81b256b71b6aec785c3972de6a619e8a157ad
zachkingcade@gmail.com <zachkingcade@gmail.com>
2021-01-30 17:38:52 -0600

    Next major upgrade

--- MyGame.js
+++ MyGame.js
@@ -1,10 +1,19 @@
 class FirstScene extends Phaser.Scene {
+    constructor(){
+        super("first");
+        this.counterText = null;
+        this.counter = 0;
+    }
+
     preload(){
 
     }
     
     create(){
-        var theRectangle = this.rect(0,0,10,10,0xff0000);
+        var RecA = this.rect(400,300,100,100,0xff0000);
+        RecA.setInteractive()
+        RecA.on("pointerdown",this.onClick,this)
+        this.counterText = this.add.text(395,295,"0", {color:'white'})
     }
 
     update(){
@@ -14,6 +23,11 @@ class FirstScene extends Phaser.Scene {
     rect(x, y, width, height, color){
         var r = this.add.rectangle(x, y, width, height, color);
         return r;
+    }
+
+    onClick(){
+        this.counter += 1;
+        this.counterText.setText(this.counter)
     }
     
 }
