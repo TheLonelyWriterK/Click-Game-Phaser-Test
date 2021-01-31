f2ece30c486f66af5ae0c3e27029f8eff0d62fb7
zachkingcade@gmail.com <zachkingcade@gmail.com>
2021-01-30 18:05:08 -0600

    Next Step

---
+++ scenes/Scene1.js
@@ -0,0 +1,37 @@
+class FirstScene extends Phaser.Scene {
+    constructor(){
+        super("first");
+        this.counterText = null;
+        this.counter = 0;
+    }
+
+    preload(){
+
+    }
+    
+    create(){
+        var RecA = this.rect(400,300,100,100,0xff0000);
+        RecA.setInteractive()
+        RecA.on("pointerdown",this.onClick,this)
+        this.counterText = this.add.text(395,295,"0", {color:'white'})
+    }
+
+    update(){
+
+    }
+
+    rect(x, y, width, height, color){
+        var r = this.add.rectangle(x, y, width, height, color);
+        return r;
+    }
+
+    onClick(){
+        this.counter += 1;
+        this.counterText.setText(this.counter)
+    }
+
+    upgrade(){
+        
+    }
+    
+}
